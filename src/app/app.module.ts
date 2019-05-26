16c1544279596ea718f4ce52da4b8cf570720ac3
Guilherme Alves <gulherme.alves@ibm.com>
2019-05-26 20:37:20 -0300

    creates database relations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatToolbarModule, MatListModule} from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -9,8 +9,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatListModule,
+    MatToolbarModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
