31989086cee1d49acf68beb690f58333828f99c9
Guilherme Alves <gulherme.alves@ibm.com>
2019-07-18 09:38:55 -0300

    handles different user validations

--- src/app/login/components/login/login.component.ts
+++ src/app/login/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +9,13 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
+  configs = {
+    isLogin: true,
+    actionText: 'SignIn',
+    buttonActionText: 'Create account'
+  }
+
+  private nameControl = new FormControl('', [Validators.required, Validators.minLength(6)])
 
   constructor(
     private formBuilder: FormBuilder
@@ -20,12 +27,29 @@ export class LoginComponent implements OnInit {
 
   createForm(): void {
     this.loginForm = this.formBuilder.group({
-      email: '',
-      password: ''
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
     })
   }
 
   onSubmit(): void {
     console.log(this.loginForm.value);
+  }
+
+  changeAction(): void {
+    this.configs.isLogin = !this.configs.isLogin;
+    this.configs.actionText = !this.configs.isLogin ? 'SignUp' : 'SignIn';
+    this.configs.buttonActionText = !this.configs.isLogin ? 'Already have an account' : 'Create account';
+    !this.configs.isLogin ? this.loginForm.addControl('name', this.nameControl) : this.loginForm.removeControl('name');
+  }
+
+  get name(): FormControl {
+    return <FormControl>this.loginForm.get('name');
+  }
+  get email(): FormControl {
+    return <FormControl>this.loginForm.get('email');
+  }
+  get password(): FormControl {
+    return <FormControl>this.loginForm.get('password');
   }
 }
