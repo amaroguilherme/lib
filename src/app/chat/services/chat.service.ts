5ff468195ee919453bd4e1377005d43a64d0bdf6
Guilherme Alves <gulherme.alves@ibm.com>
2019-08-21 18:35:53 -0300

    resolve guard configurations for meetings window component

--- src/app/chat/services/chat.service.ts
+++ src/app/chat/services/chat.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { Observable } from 'rxjs';
-import { AllChatsQuery, USER_CHATS_QUERY } from './chat.graphql';
+import { AllChatsQuery, USER_CHATS_QUERY, ChatQuery, CHAT_BY_ID_OR_USERS_QUERY } from './chat.graphql';
 import { map } from 'rxjs/operators';
 import { Chat } from '../models/chat.model';
 
@@ -23,5 +23,16 @@ export class ChatService {
         userId: this.authService.authUser.id
       }
     }).pipe(map(res => res.data.allChats));
+  }
+
+  getChatByIdOrUsers(chatOrUserId: string): Observable<Chat> {
+    return this.apollo.query<ChatQuery | AllChatsQuery>({
+      query: CHAT_BY_ID_OR_USERS_QUERY,
+      variables: {
+        chatId: chatOrUserId,
+        loggedUserId: this.authService.authUser.id,
+        targetUserId: chatOrUserId
+      }
+    }).pipe(map(res => (res.data['Chat']) ? res.data['Chat'] : res.data['allChats'][0]));
   }
 }
